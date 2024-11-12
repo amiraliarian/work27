1756066ac2b6e92617097f81c4c61d8a44ec27e7
amirali <amiraliariyan4444@gmail.com>
2024-11-12 15:25:39 +0330

    2

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-function randomBg(){}
+function randomBg(){for(let n=1; n<=6 ;n++){
+        
+    let char1=0
+    let char2=0
+    let char3=0
+    
+    char1+=Math.floor(Math.random()*256);
+    char2+=Math.floor(Math.random()*256);
+    char3+=Math.floor(Math.random()*256);
+       
+
+    if(char1!==char2&&char1!==char3&&char2!==char3){
+    document.getElementById(`items${n}`).style=(`background-color:rgb(${char1},${char2},${char3});`);
+    }   
+
+}
+return document;
+
+  
+};
+randomBg();}
